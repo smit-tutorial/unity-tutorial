792162a96313210e7246a7e496188126f272bf77
Yeojin Kim <yeojin.dev@gmail.com>
2019-05-29 17:21:59 +0900

    버튼 클릭 시 딜레이 후 씬 변경하는 스크립트 추가 #1

---
+++ unity-tutorial/Assets/Scripts/SceneChangeButtonCtrl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneChangeButtonCtrl : MonoBehaviour
+{
+    public float waitingTime = 3.0f;
+    public int sceneNumber;
+
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(() =>
+        {
+            StartCoroutine(DelayedChangeScene(waitingTime, sceneNumber));
+        });
+    }
+
+    IEnumerator DelayedChangeScene(float time, int number)
+    {
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(number);
+    }
+}
