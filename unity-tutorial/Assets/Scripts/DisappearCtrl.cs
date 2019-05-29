373a98b4e4a272dd8f1dee3a60338975270b1808
Yeojin Kim <yeojin.dev@gmail.com>
2019-05-29 17:25:03 +0900

    Add scripts to images in IntroScene #2

---
+++ unity-tutorial/Assets/Scripts/DisappearCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearCtrl : MonoBehaviour
+{
+    public float waitingTime = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Disappear(waitingTime));
+    }
+
+    // Update is called once per frame
+    IEnumerator Disappear(float time)
+    {
+        yield return new WaitForSeconds(time);
+        this.gameObject.SetActive(false);
+    }
+}
