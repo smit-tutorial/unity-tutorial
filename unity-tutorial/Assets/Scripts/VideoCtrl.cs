d20c84750a36ab211e025772e1a88690381f527f
Yeojin Kim <yeojin.dev@gmail.com>
2019-06-09 20:08:34 +0900

    Add VideoPlayer close #4

---
+++ unity-tutorial/Assets/Scripts/VideoCtrl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoCtrl : MonoBehaviour
+{
+    private VideoPlayer vp;
+    private RawImage rawImage;
+
+    void Start()
+    {
+        vp = GetComponent<VideoPlayer>();
+        rawImage = GetComponent<RawImage>();
+    }
+
+    public void VideoPlay()
+    {
+        vp.Play();
+    }
+
+    public void VideoStop()
+    {
+        vp.Pause();
+    }
+
+    public void VideoReplay()
+    {
+        vp.Stop();
+        vp.Play();
+    }
+}
