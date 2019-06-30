3527207a1300a7f16ed929ef33d7df4dedf2c9dd
Yeojin Kim <yeojin.dev@gmail.com>
2019-07-01 07:23:17 +0900

    Add DontDestroy script

---
+++ unity-tutorial/Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    // Do not destroy the target Object when loading a new Scene
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
